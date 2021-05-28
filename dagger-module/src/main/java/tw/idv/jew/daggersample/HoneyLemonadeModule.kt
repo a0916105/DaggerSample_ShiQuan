22c26cad7a8a0ea699bebfd24815ac18701a223c
a0916105 <whdofjew@gmail.com>
2021-05-28 14:05:53 +0800

    Dagger的Binds

--- dagger-module/src/main/java/tw/idv/jew/daggersample/HoneyLemonadeModule.kt
+++ dagger-module/src/main/java/tw/idv/jew/daggersample/HoneyLemonadeModule.kt
@@ -1,12 +1,16 @@
 package tw.idv.jew.daggersample
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module
-class HoneyLemonadeModule {
-    @Provides
+//class HoneyLemonadeModule {
+abstract class HoneyLemonadeModule {
+    /*@Provides
     fun provideLemon(lyme: Lyme): Lemon {
         return lyme
-    }
+    }*/
+    @Binds
+    abstract fun provideLemon(lyme: Lyme): Lemon
 }
