f530cb84d3309743b93922e5c58063fbb634d5cd
a0916105 <whdofjew@gmail.com>
2021-06-04 15:18:05 +0800

    multibindings.IntoMap (不完整範例)

--- dagger-multi-binding/src/main/java/tw/idv/jew/daggersample/HoneyLemonadeComponent.kt
+++
@@ -1,8 +0,0 @@
-package tw.idv.jew.daggersample
-
-import dagger.Component
-
-@Component(modules = [HoneyLemonadeModule::class])
-interface HoneyLemonadeComponent {
-    fun inject(mainActivity: MainActivity)
-}
--- dagger-multi-binding/src/main/java/tw/idv/jew/daggersample/MainActivity.kt
+++ dagger-multi-binding/src/main/java/tw/idv/jew/daggersample/MainActivity.kt
@@ -2,19 +2,28 @@ package tw.idv.jew.daggersample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
     @Inject
-    lateinit var set: Set<HoneyLemonade>
+    lateinit var viewModelFactory: GeneralViewModelFactory
+
+    private lateinit var viewModel: AViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        DaggerHoneyLemonadeComponent.create().inject(this)
+        AndroidInjection.inject(this)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView.text = set.toString()
+        viewModel = ViewModelProvider(this, viewModelFactory).get(AViewModel::class.java)
+
+        viewModel.messageLiveData.observe(this, Observer<String>{
+            textView.text = it
+        })
     }
 }
