014a0e4156beae00a228355f7b7ba130377068c7
a0916105 <whdofjew@gmail.com>
2021-06-04 17:27:05 +0800

    hilt的Context

--- hilt-basic/src/main/java/tw/idv/jew/daggersample/HoneyLemonade.kt
+++ hilt-basic/src/main/java/tw/idv/jew/daggersample/HoneyLemonade.kt
@@ -1,15 +1,23 @@
 package tw.idv.jew.daggersample
 
+import android.content.Context
+import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 import javax.inject.Singleton
 
 class HoneyLemonade @Inject constructor(val honey: Honey, val lemon: Lemon)
 
-class Honey @Inject constructor(val bee: Bee)
+class Honey @Inject constructor(
+    @ActivityContext context: Context,
+    val bee: Bee
+)
 
 @Singleton
-class Bee @Inject constructor()
+class Bee @Inject constructor(
+    @ApplicationContext context: Context
+)
 
 @ActivityScoped
 open class Lemon @Inject constructor()
